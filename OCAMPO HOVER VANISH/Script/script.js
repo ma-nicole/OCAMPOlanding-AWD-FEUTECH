f4f14d83a6e3619f29ad40ab05cf1dab06f7af6b
ma-nicole <155798589+ma-nicole@users.noreply.github.com>
2024-03-09 13:26:34 +0800

    Delete OCAMPO HOVER VANISH/Script directory

--- OCAMPO HOVER VANISH/Script/script.js
+++
@@ -1,7 +0,0 @@
-for (let index = 0; index < 2000; index++) {
-  const element = document.createElement("span");
-  element.addEventListener('mouseover', function() {
-    this.classList.add('permanent');
-  });
-  document.querySelector(".container").appendChild(element);
-}
